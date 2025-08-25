bfec481816e3836bd500c90db289c618541fd692
Jasper Smet <josbeir@gmail.com>
2025-08-25 19:00:20 +0200

    Remove unneeded plugin overrides

--- src/LatteViewPlugin.php
+++ src/LatteViewPlugin.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace LatteView;
 
-use Cake\Console\CommandCollection;
 use Cake\Core\BasePlugin;
 
 /**
@@ -11,18 +10,4 @@ use Cake\Core\BasePlugin;
  */
 class LatteViewPlugin extends BasePlugin
 {
-    /**
-     * Add commands for the plugin.
-     *
-     * @param \Cake\Console\CommandCollection $commands The command collection to update.
-     */
-    public function console(CommandCollection $commands): CommandCollection
-    {
-        // Add your commands here
-        // remove this method hook if you don't need it
-
-        $commands = parent::console($commands);
-
-        return $commands;
-    }
 }
